0610545dd6df26eda7153172cca5625ed0c01e7f
duy010203N <125200444+duy010203N@users.noreply.github.com>
2023-04-06 06:54:53 +0700

    Update TiviTivi.java

--- TiviTivi.java
+++ TiviTivi.java
@@ -1,105 +1,17 @@
 package DAYDU;
 import java.util.ArrayList; // lưu trữ danh 
 import java.util.Collections;   //sắp xếp danh sách các số nguyên theo thứ tự tăng dần.
-import java.util.Comparator;
+import java.util.Comparator;    //sắp xếp danh sách các số nguyên theo thứ tự giảm dần.
 import java.util.Scanner;
-import java.util.Iterator;
-import java.util.List;
+import java.util.Iterator;      //lặp lại các phần tử của một collection( phương thức : hasnext,next,
+import java.util.List;          //Các phương thức này được triển khai bởi lớp ArrayList dựa trên interface List.(phương thức : thêm, xóa, sắp xếp, truy cập phần tử trong danh sách,)
 
 public class Tivi {
     
-        this.mativi = mativi;                     //
-        this.loaitivi = loaitivi;
-        this.kieumanhinh = kieumanhinh;
-        this.hangsanxuat = hangsanxuat;
-        this.kichthuoctivi = kichthuoctivi;
-        this.dongia = dongia;
-        this.soluong = soluong;
+        this.mativi = mativi;                     //gán các giá trị đầu vào cho các thuộc tính tương ứng
+      
     }
 
-    public String getMativi() {
-       return mativi;
-    }
-
-    public void setMativi(String mativi) {
-        this.mativi = mativi;
-    }
-
-    public String getloaitivi() {
-        return loaitivi;
-    }
-    public void setloaitivi(String loaitivi){
-        this.loaitivi=loaitivi;
-    }
-
-    public void setTentivi(String loaitivi) {
-        this.loaitivi = loaitivi;
-    }
-
-    public String getkieumanhinh() {
-        return kieumanhinh;
-    }
-
-    public void setkieumahinh(String kieumanhinh) {
-        this.kieumanhinh = kieumanhinh;
-    }
-    public String gethangsanxuat() {
-        return hangsanxuat;
-    }
-    public void sethangsanxuat(String hangsanxuat){
-        this.hangsanxuat=hangsanxuat;
-    }
-
-    public double getkichthuoctivi() {
-        return kichthuoctivi;
-    }
-
-    public void setkichthuoctivi(double kichthuoctivi) {
-        this.kichthuoctivi = kichthuoctivi;
-    }
-
-    public double getdongia() {
-        return dongia;
-    }
-
-    public void setdongia(double dongia) {
-        this.dongia = dongia;
-    }
-
-    public int getsoluong() {
-        return soluong;
-    }
-
-    public void setsoluong(int soluong) {
-        this.soluong = soluong;
-    }
-
-    public void nhap() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhập mã Tivi:");
-        mativi = scanner.nextLine();
-        System.out.println("Nhập loại Tivi:");
-        loaitivi = scanner.nextLine();
-        System.out.println("Nhập kiểu màn hình:");
-        kieumanhinh = scanner.nextLine();
-        System.out.println("Nhập kích thước Tivi:");
-        kichthuoctivi = scanner.nextDouble();
-        System.out.println("Nhập hãng sản xuất:");
-        hangsanxuat = scanner.nextLine();
-        System.out.println("Nhập đơn giá:");
-        dongia = scanner.nextDouble();
-        System.out.println("Nhập số lượng:");
-        soluong = scanner.nextInt();
-    }
-       public void xuat(){
-        System.out.println("mã Tivi:"+ mativi);
-        System.out.println("loại Tivi:"+ loaitivi);
-        System.out.println("Kiểu màn hình:"+ kieumanhinh);
-        System.out.println("Hãng sản xuất:"+ hangsanxuat);
-        System.out.println("Kích thước Tivi:"+ kichthuoctivi);
-        System.out.println("đơn giá :"+ dongia);
-        System.out.println("số lượng:"+ soluong);
-       }
 }
 2.Class TiVi OLED
 class tivioled extends Tivi{
@@ -109,31 +21,7 @@ public tivioled() {
 public tivioled(String mativi, String kieumanhinh, double kichthuoctivi, String hangsanxuat, double dongia, int soluong ) {
 super(mativi, kieumanhinh,kichthuoctivi,hangsanxuat ,dongia,soluong);
 }
-
-@Override
-public void nhap() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhập mã Tivi:");
-        mativi = scanner.nextLine();
-        System.out.println("Nhập Hãng sản xuất:");
-        hangsanxuat = scanner.nextLine();
-        System.out.println("Nhập kiểu màn hình:");
-        kieumanhinh = scanner.nextLine();
-        System.out.println("Nhập kích thước Tivi:");
-        kichthuoctivi = scanner.nextDouble();
-        System.out.println("Nhập đơn giá:");
-        dongia = scanner.nextDouble();
-        System.out.println("Nhập số lượng:");
-        soluong = scanner.nextInt();
-    }
-@Override
-       public void xuat(){
-        System.out.println("mã Tivi:"+ mativi);
-        System.out.println("Hãng sản xuất:"+ hangsanxuat);
-        System.out.println("Kiểu màn hình:"+ kieumanhinh);
-        System.out.println("Kích thước Tivi:"+ kichthuoctivi);
-        System.out.println("đơn giá:"+ dongia);
-        System.out.println("số lượng:"+ soluong);
+// có thể super.xuat();// ghđèđè
        }
 }
 3.Class TiVi 4K
@@ -141,153 +29,56 @@ class TV4k extends Tivi{
 
 public TV4k() {
 }
-public TV4k(String mativi, String hangsanxuat, double kichthuoctivi,String kieumanhinh,double dongia, int soluong ) {
-super(mativi,hangsanxuat,kieumanhinh,kichthuoctivi,dongia,soluong);
-}
 
-@Override
-public void nhap() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhập mã Tivi:");
-        mativi = scanner.nextLine();
-        System.out.println("Nhập Hãng sản xuất:");
-        hangsanxuat= scanner.nextLine();
-        System.out.println("Nhập kiểu màn hình:");
-        kieumanhinh = scanner.nextLine();
-        System.out.println("Nhập kích thước Tivi:");
-        kichthuoctivi = scanner.nextDouble();
-        System.out.println("Nhập đơn giá :");
-        dongia = scanner.nextDouble();
-        System.out.println("Nhập số lượng:");
-        soluong = scanner.nextInt();
-    }
-@Override
-       public void xuat(){
-        System.out.println("mã Tivi:"+ mativi);
-        System.out.println("Hãng sản xuất:"+ hangsanxuat);
-        System.out.println("Kiểu màn hình:"+ kieumanhinh);
-        System.out.println("Kích thước Tivi:"+ kichthuoctivi);
-        System.out.println("đơn giá:"+ dongia);
-        System.out.println("số lượng:"+ soluong);
-       }
 } 
 4.Class Menu chính
 class Menuchinh {
 
-    List<Tivi> tvList = new ArrayList<>();
-    boolean menu12 = false;
+    List<Tivi> tvList = new ArrayList<>();      // khai báo một danh sách các đối tượng Tivi với kiểu dữ liệu là List<Tivi>. Đối tượng ArrayList này được khởi tạo và lưu trữ trong biến tvList
+    boolean menu12 = false; //để xác định menu đang được chọn, có giá trị là true nếu menu 1 hoặc 2 được chọn, và false nếu menu 3 hoặc 4 được chọn.
     boolean menu34 = false;
     Scanner sc = new Scanner(System.in);
-
-    public void displayMenu12() {
-        System.out.println("------CHƯƠNG TRÌNH QUẢN LÝ KHO TIVI------");
-        System.out.println("Mời bạn chọn : ");
-        System.out.println("=== Menu 1 ===");
-        System.out.println("1. Tivi OLED");
-        System.out.println("2. Tivi 4K");
-        System.out.println("0. thoát");
-        
     }
 
-    public void displayMenu34() {
-        System.out.println("=== Menu 2 ===");
-        System.out.println("1. thêm vào danh sách ");
-        System.out.println("2. hiện thị danh sách ");
-        System.out.println("3. Xóa khỏi danh sách ");
-        System.out.println("4. Sửa thông tin trong danh sách ");
-        System.out.println("5. Sắp xếp theo đơn giá ");
-        System.out.println("6.Quay lai Menu 1");
-        System.out.println("Mời bạn chọn: ");
-
-    }
-
-    public void selectMenu() {
-    int choice; 
-    do {
-        
-        if (!menu12) {
-            displayMenu12();
-                choice = sc.nextInt();
-                switch (choice) {
-                    case 1:
-                    menu12 = true;
-                        break;
-                    case 2:
-                        menu12 =true;
-                        break;
-                    case 0:
-                       System.out.println("đang thoát ...!");
-                    return;
-                default:
-                    
-                    System.out.println("Lựa chọn không hợp lệ, vui lòng chọn lại.");
-                    break;
-                  
-                }
-        } else if (!menu34) {
-            boolean menu34 = false;
-           
-        do{
-            displayMenu34();
-            choice = sc.nextInt();
-            switch (choice) {
+   
                 case 1:
     System.out.print("Nhập số lượng cần thêm: ");
-    int n = sc.nextInt();
+    int n = sc.nextInt(); // yêu cầu nhập vào giá trị nguyên và lưu biến đó vào n
     for (int i = 0; i < n; i++) {
         System.out.println("lần nhập thứ " + (i + 1) + ": ");
         Scanner input = new Scanner(System.in); // tạo đối tượng Scanner mới
         // Thêm Tivi
-        System.out.println("Nhập mã Tivi:");
-        String maTivi = input.nextLine();
+      
 
-        System.out.println("Nhập Hãng sản xuất :");
-        String hangsanxuat = input.next();
-
-        System.out.println("Nhập kiểu màn hình:");
-        String kieumanhinh = input.next();
-        
-        System.out.println("Nhập kích thước Tivi:");
-        double kichthuoctivi = input.nextDouble();
-
-        
-        System.out.println("Nhập đơn giá:");
-        double donGia = input.nextDouble();
-
-        System.out.println("Nhập số lượng:");
-        int soLuong = input.nextInt();
-
-       Tivi tivi = new Tivi(maTivi, loaiTivi, kieumanhinh, kichthuoctivi, donGia, soLuong);
-        tvList.add(tivi);
-       
-        System.out.println("Thêm Tivi Thành Công!");
+       Tivi tivi = new Tivi(maTivi, loaiTivi, kieumanhinh, kichthuoctivi, donGia, soLuong);// tạo ra các đối tượng tivi mới
+        tvList.add(tivi); // thêm đối tượng vào danh sách(list)
+    
     }
            
-    menu34 = true;
-    break;
+    menu34 = true;// chỉ ra rằng mã bên trong switch khối hiện tại đã thực thi thành công và chương trình sẽ chuyển sang khối mã tiếp theo. 
+    break; // dùng để thoát khỏi khối chuyển đổi sau khi thựuc hiện các câu lệnh trong một case, còn nếu bỏ đi thì khi thực hiện xong case mình chọn thì nó sẽ tự động chuyển case tiếp 
                 
  
 case 2:
     // Hiển thị danh sách tivi
-    if (tvList.isEmpty()) {
+    if (tvList.isEmpty()) {// tvList là biến lưu trữ danh sách các đối tượng
+                            // isEmpty()thức được gọi trong danh sách này để xác định xem nó có rỗng hay không.
         System.out.println("Danh sách trống.");
     } else {
         System.out.println("Danh sách Tivi:");
-        for (Tivi tivi : tvList) {
+        for (Tivi tivi : tvList) {  // truy cập trực tiếp vào phần tử hiện tại của tvListviệc sử dụng biến tivi
+                                    // Vòng lặp lặp lại tvListdanh sách, có lẽ chứa các đối tượng của Tivilớp và gán từng phần tử trong danh sách cho tivimột biến tại một thời điểm.
             tivi.xuat();
         }
          menu34 = true;
     }
     break;
 case 3:
-   if (tvList.isEmpty()) {
-        System.out.println("Danh sách trống.");
-    } else {
-Scanner sc = new Scanner(System.in);
-        System.out.println("Nhập mã Tivi cần xóa:");
-        String maTiviXoa = sc.nextLine();
+   
         boolean timThay = false;
         for (Iterator<Tivi> iterator = tvList.iterator(); iterator.hasNext();) {
+            //Khởi tạo một biến iterator kiểu Iterator<Tivi> và gán cho nó giá trị bằng cách sử dụng phương thức iterator() của danh sách tvList.
+Khởi tạo điều kiện để lặp lại vòng lặp: nếu phương thức hasNext() của iterator trả về true, vòng lặp sẽ tiếp tục. Nếu phương thức này trả về false, vòng lặp sẽ kết thúc.
             Tivi tivi = iterator.next();
             if (tivi.getMativi() == maTiviXoa) {
                 iterator.remove();
@@ -313,27 +104,7 @@ case 4:
     for (Iterator<Tivi> iterator = tvList.iterator(); iterator.hasNext();) {
         Tivi tivi = iterator.next();
         if (tivi.getMativi().equalsIgnoreCase(maTiviSua)) {
-            System.out.println("Nhập thông tin Tivi mới:");
-            
-            System.out.println("Nhap ma tivi: ");
-            String mativi = sc.nextLine();
-              
-            System.out.println("Nhập Hãng sản xuất:");
-            String hangsanxuat = sc.nextLine();
-            
-            System.out.println("Nhập kiểu màn hình:");
-            String kieumanhinh = sc.nextLine();
-
-            System.out.println("Nhập kích thước Tivi:");
-            Double kichthuoctivi = sc.nextDouble();
-            
-            System.out.println("Nhập đơn giá:");
-            Double dongia = sc.nextDouble();
-            
-            System.out.println("Nhập số lượng:");
-                    
-            int soluong = sc.nextInt();
-                   
+          
             
 
             // Cập nhật thông tin Tivi   
